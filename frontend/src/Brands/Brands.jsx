db54dbe44fc4d3fb64643d21cb749890773284dc
sujith surendran <sujithsurendran4433@gmail.com>
2024-11-13 10:43:55 +0530

    added till now

--- frontend/src/Brands/Brands.jsx
+++ frontend/src/Brands/Brands.jsx
@@ -1,10 +1,61 @@
 import React from 'react'
+import { Slash } from "lucide-react"
+import { phoneData } from '@/phoneData'
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from '@/components/ui/breadcrumb'
 
 const Brands = () => {
   return (
-    <div>
-      this is brand
-    </div>
+    <>
+      <section >
+        <div className='containerrr  flex relative w-full'>
+          <div className='hidden sm:flex border border-red-500 w-[20%] '>
+            <div className='p-3'>
+              <div className='' >
+                <div className='font-semibold text-2xl pb-5'>Product Filter</div>
+                <div>
+                  <ul>
+                    {
+                      phoneData.map((phone, index) => (
+
+                        <li className='flex gap-2' key={index} >
+                          <input type="checkbox" name="checkbox" id="" />
+                          <label htmlFor="checkbox">{phone.brand}</label>
+                        </li>
+
+                      ))
+                    }
+
+                  </ul>
+                </div>
+              </div>
+
+            </div>
+          </div>
+          <div className='border border-green-500 w-full flex-col '>
+            <div className='flex justify-center p-2 text-[#20b2aa] '>
+              <h2 className='font-bold text-6xl'>Shop</h2>
+            </div>
+            <div className='p-3'>
+              <Breadcrumb>
+                <BreadcrumbList>
+                  <BreadcrumbItem>
+                    <BreadcrumbLink href="/">Home</BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator>
+                    <Slash />
+                  </BreadcrumbSeparator>
+                  <BreadcrumbItem>
+                    <BreadcrumbLink href="/brands">Shop</BreadcrumbLink>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+            </div>
+            <div></div>
+
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
---
+++ frontend/src/phoneData.js
@@ -0,0 +1,171 @@
+export const phoneData = [
+    {
+        brand: "Asus",
+        models: [
+            {
+                name: "Zenfone 8",
+                battery: "4000mAh Li-Po"
+            },
+            {
+                name: "ROG Phone 5",
+                battery: "6000mAh Li-Po"
+            }
+        ]
+    },
+    {
+        brand: "Honor",
+        models: [
+            {
+                name: "Honor 50",
+                battery: "4300mAh Li-Po"
+            },
+            {
+                name: "Honor Magic4 Pro",
+                battery: "4600mAh Li-Po"
+            }
+        ]
+    },
+    {
+        brand: "Huawei",
+        models: [
+            {
+                name: "P50 Pro",
+                battery: "4360mAh Li-Po"
+            },
+            {
+                name: "Mate 40 Pro",
+                battery: "4400mAh Li-Po"
+            }
+        ]
+    },
+    {
+        brand: "iPhone",
+        models: [
+            {
+                name: "iPhone 13 Pro Max",
+                battery: "4352mAh Li-Ion"
+            },
+            {
+                name: "iPhone 14 Pro",
+                battery: "3200mAh Li-Ion"
+            }
+        ]
+    },
+    {
+        brand: "Lenovo",
+        models: [
+            {
+                name: "Legion Phone Duel 2",
+                battery: "5500mAh Li-Po"
+            },
+            {
+                name: "K13 Note",
+                battery: "5000mAh Li-Po"
+            }
+        ]
+    },
+    {
+        brand: "LG",
+        models: [
+            {
+                name: "V60 ThinQ",
+                battery: "5000mAh Li-Po"
+            },
+            {
+                name: "Wing 5G",
+                battery: "4000mAh Li-Po"
+            }
+        ]
+    },
+    {
+        brand: "MI",
+        models: [
+            {
+                name: "Mi 12 Pro",
+                battery: "4600mAh Li-Po"
+            },
+            {
+                name: "Redmi Note 11",
+                battery: "5000mAh Li-Po"
+            }
+        ]
+    },
+    {
+        brand: "Motorola",
+        models: [
+            {
+                name: "Edge 30 Pro",
+                battery: "4800mAh Li-Po"
+            },
+            {
+                name: "Moto G Power (2022)",
+                battery: "5000mAh Li-Po"
+            }
+        ]
+    },
+    {
+        brand: "Nokia",
+        models: [
+            {
+                name: "X30 5G",
+                battery: "4200mAh Li-Po"
+            },
+            {
+                name: "G60 5G",
+                battery: "4500mAh Li-Po"
+            }
+        ]
+    },
+    {
+        brand: "OnePlus",
+        models: [
+            {
+                name: "10 Pro",
+                battery: "5000mAh Li-Po"
+            },
+            {
+                name: "Nord 2T",
+                battery: "4500mAh Li-Po"
+            }
+        ]
+    },
+    {
+        brand: "Oppo",
+        models: [
+            {
+                name: "Find X5 Pro",
+                battery: "5000mAh Li-Po"
+            },
+            {
+                name: "Reno8 Pro",
+                battery: "4500mAh Li-Po"
+            }
+        ]
+    },
+    {
+        brand: "Samsung",
+        models: [
+            {
+                name: "Galaxy S23 Ultra",
+                battery: "5000mAh Li-Ion"
+            },
+            {
+                name: "Galaxy A53 5G",
+                battery: "5000mAh Li-Po"
+            }
+        ]
+    },
+    {
+        brand: "Vivo",
+        models: [
+            {
+                name: "X80 Pro",
+                battery: "4700mAh Li-Po"
+            },
+            {
+                name: "V25 Pro",
+                battery: "4830mAh Li-Po"
+            }
+        ]
+    }
+];
